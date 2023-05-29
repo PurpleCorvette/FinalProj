8a0fd2cbe8967c4d49aca1065b809bba711432fc
PurpleCorvette <lesoboleff@mail.ru>
2023-05-30 01:09:30 +0300

    Добавляю класс LogoutController

---
+++ src/main/java/com/example/finalproj/controllers/LogoutController.java
@@ -0,0 +1,27 @@
+package com.example.finalproj.controllers;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class LogoutController {
+
+    //Метод завершает текущую сессию и редиректится на шаблон authentication при условии,
+    //отключен csrf сертификат в шаблоне и в настройках SecurityConfig
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null) {
+            new SecurityContextLogoutHandler().logout(request, response, auth);
+        }
+        return "redirect:/login?logout"; // перенаправление на страницу входа с сообщением о выходе
+    }
+
+}
